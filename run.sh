9be263e720c91ad65473490d94579c2c5e2494b5
simonr-1 <87853741+simonr-1@users.noreply.github.com>
2021-08-11 14:07:17 +0100

    Update run.sh
    add record to table before running job

--- run.sh
+++ run.sh
@@ -2,6 +2,11 @@
 
 #awslocal s3 mb s3://test
 #awslocal s3 cp etl.py s3://test/etl.py
-
 #awslocal glue create-job --name python-job-cli --role Glue_DefaultRole --command '{"Name" : "my_python_etl", "ScriptLocation" : "s3://test/etl.py"}'
+
+record="{\"field_a\": {\"S\": \"gdfgfdgf\"}, \"id\": {\"N\": \"${RANDOM}\"},\"timestamp\": {\"N\": \"1626378466618021000\"}}"
+awslocal dynamodb put-item \
+    --table-name test_table  \
+    --item \
+        "$record"
 awslocal glue start-job-run --job-name python-job-cli
